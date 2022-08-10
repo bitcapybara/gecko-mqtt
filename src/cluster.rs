ab835de7db34b989b66ec5c2c611fbd018a6815e
bitcapybara <1227686398@qq.com>
2022-08-10 21:56:17 +0800

    注释

--- src/cluster.rs
+++ src/cluster.rs
@@ -3,7 +3,8 @@
 //! 当前有三种方式：
 //! * 单机版本
 //! * 少量节点：使用 raft 算法
-//! * 多节点：借助第三方一致性存储，如 Etcdmod standalone;
+//! * 多节点：借助第三方一致性存储，如 Etcd
+//! * Etcd 模式，数据在本地没有备份，可能会增加访问数据的延迟
 
 // pub(crate) use connection::Connection;
 pub(crate) use dispatcher::Dispatcher;
