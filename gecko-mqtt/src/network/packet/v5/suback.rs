cfadb4b9d703bfa89668d343c0e3c2249716c388
yanyuxing <yanyuxing@aiforward.com>
2022-09-06 11:38:55 +0800

    suback write

--- gecko-mqtt/src/network/packet/v5/suback.rs
+++ gecko-mqtt/src/network/packet/v5/suback.rs
@@ -1,3 +1,7 @@
+use bytes::{BufMut, BytesMut};
+
+use crate::network::packet::{self, Error};
+
 use super::PacketProperties;
 
 #[derive(Debug)]
@@ -7,7 +11,47 @@ pub struct SubAck {
     pub properties: Option<PacketProperties>,
 }
 
-#[derive(Debug)]
+impl SubAck {
+    fn len(&self) -> usize {
+        let mut len = 2 + self.return_codes.len();
+
+        match &self.properties {
+            Some(properties) => {
+                let properties_len = properties.len();
+                let properties_len_len = super::len_len(properties_len);
+                len += properties_len_len + properties_len;
+            }
+            None => len += 1,
+        }
+
+        len
+    }
+
+    pub fn write(&self, stream: &mut BytesMut) -> Result<(), Error> {
+        stream.put_u8(0x90);
+        packet::write_remaining_length(stream, self.len())?;
+
+        stream.put_u16(self.packet_id);
+
+        match &self.properties {
+            Some(properties) => properties.write(stream)?,
+            None => {
+                packet::write_remaining_length(stream, 0)?;
+            }
+        }
+
+        let codes = self
+            .return_codes
+            .iter()
+            .map(|code| *code as u8)
+            .collect::<Vec<u8>>();
+        stream.extend_from_slice(&codes);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum SubscribeReasonCode {
     QoS0 = 0,
@@ -22,4 +66,28 @@ pub enum SubscribeReasonCode {
     SharedSubscriptionsNotSupported = 158,
     SubscriptionIdNotSupported = 161,
     WildcardSubscriptionsNotSupported = 162,
+}
+
+impl TryFrom<u8> for SubscribeReasonCode {
+    type Error = super::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        let v = match value {
+            0 => SubscribeReasonCode::QoS0,
+            1 => SubscribeReasonCode::QoS1,
+            2 => SubscribeReasonCode::QoS2,
+            128 => SubscribeReasonCode::Unspecified,
+            131 => SubscribeReasonCode::ImplementationSpecific,
+            135 => SubscribeReasonCode::NotAuthorized,
+            143 => SubscribeReasonCode::TopicFilterInvalid,
+            145 => SubscribeReasonCode::PkidInUse,
+            151 => SubscribeReasonCode::QuotaExceeded,
+            158 => SubscribeReasonCode::SharedSubscriptionsNotSupported,
+            161 => SubscribeReasonCode::SubscriptionIdNotSupported,
+            162 => SubscribeReasonCode::WildcardSubscriptionsNotSupported,
+            v => return Err(super::Error::InvalidReasonCode(v)),
+        };
+
+        Ok(v)
+    }
 }
