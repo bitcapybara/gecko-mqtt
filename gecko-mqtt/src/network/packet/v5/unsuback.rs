321e441060dc14d4ddcbc978f6274de54d2b9e80
yanyuxing <yanyuxing@aiforward.com>
2022-09-06 14:24:56 +0800

    unsub read write

--- gecko-mqtt/src/network/packet/v5/unsuback.rs
+++ gecko-mqtt/src/network/packet/v5/unsuback.rs
@@ -1,3 +1,7 @@
+use bytes::{BufMut, BytesMut};
+
+use crate::network::packet::{self, Error};
+
 use super::PacketProperties;
 
 #[derive(Debug)]
@@ -7,7 +11,47 @@ pub struct UnsubAck {
     pub properties: Option<PacketProperties>,
 }
 
-#[derive(Debug)]
+impl UnsubAck {
+    fn len(&self) -> usize {
+        let mut len = 2 + self.reasons.len();
+
+        match &self.properties {
+            Some(properties) => {
+                let properties_len = properties.len();
+                let properties_len_len = super::len_len(properties_len);
+                len += properties_len_len + properties_len;
+            }
+            None => len += 1,
+        }
+
+        len
+    }
+
+    pub fn write(&self, stream: &mut BytesMut) -> Result<(), Error> {
+        stream.put_u8(0x80);
+        packet::write_remaining_length(stream, self.len())?;
+
+        stream.put_u16(self.packet_id);
+
+        match &self.properties {
+            Some(properties) => properties.write(stream)?,
+            None => {
+                packet::write_remaining_length(stream, 0)?;
+            }
+        }
+
+        let codes = self
+            .reasons
+            .iter()
+            .map(|code| *code as u8)
+            .collect::<Vec<u8>>();
+        stream.extend_from_slice(&codes);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum UnsubAckReason {
     Success = 0x00,
@@ -17,4 +61,23 @@ pub enum UnsubAckReason {
     NotAuthorized = 0x87,
     TopicFilterInvalid = 0x8F,
     PacketIdentifierInUse = 0x91,
+}
+
+impl TryFrom<u8> for UnsubAckReason {
+    type Error = super::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        let code = match value {
+            0x00 => UnsubAckReason::Success,
+            0x11 => UnsubAckReason::NoSubscriptionExisted,
+            0x80 => UnsubAckReason::UnspecifiedError,
+            0x83 => UnsubAckReason::ImplementationSpecificError,
+            0x87 => UnsubAckReason::NotAuthorized,
+            0x8F => UnsubAckReason::TopicFilterInvalid,
+            0x91 => UnsubAckReason::PacketIdentifierInUse,
+            num => return Err(super::Error::InvalidReasonCode(num)),
+        };
+
+        Ok(code)
+    }
 }
--- gecko-mqtt/src/network/packet/v5/unsubscribe.rs
+++ gecko-mqtt/src/network/packet/v5/unsubscribe.rs
@@ -1,3 +1,9 @@
+use bytes::{Buf, Bytes};
+
+use crate::network::packet::{self, Error};
+
+use super::PropertyType;
+
 #[derive(Debug)]
 pub struct Unsubscribe {
     pub packet_id: u16,
@@ -5,7 +11,56 @@ pub struct Unsubscribe {
     pub properties: Option<UnsubscribeProperties>,
 }
 
+impl Unsubscribe {
+    pub fn read(mut stream: Bytes) -> Result<Self, Error> {
+        let packet_id = packet::read_u16(&mut stream)?;
+        let properties = UnsubscribeProperties::read(&mut stream)?;
+
+        let mut filters = Vec::new();
+        while stream.has_remaining() {
+            let filter = packet::read_string(&mut stream)?;
+            filters.push(filter);
+        }
+
+        Ok(Self {
+            packet_id,
+            filters,
+            properties,
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct UnsubscribeProperties {
     pub user_properties: Vec<(String, String)>,
+}
+
+impl UnsubscribeProperties {
+    fn read(stream: &mut Bytes) -> Result<Option<Self>, Error> {
+        let mut user_properties = Vec::new();
+
+        let (properties_len_len, properties_len) = packet::length(stream.iter())?;
+        stream.advance(properties_len_len);
+        if properties_len == 0 {
+            return Ok(None);
+        }
+
+        let mut cursor = 0;
+        while cursor < properties_len {
+            let prop = packet::read_u8(stream)?;
+            cursor += 1;
+
+            match prop.try_into()? {
+                PropertyType::UserProperty => {
+                    let key = packet::read_string(stream)?;
+                    let value = packet::read_string(stream)?;
+                    cursor += 2 + key.len() + 2 + value.len();
+                    user_properties.push((key, value));
+                }
+                _ => return Err(Error::InvalidPacketType(prop)),
+            }
+        }
+
+        Ok(Some(Self { user_properties }))
+    }
 }
